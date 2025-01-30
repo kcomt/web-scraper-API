440577e9d51639e56917a64f943d945b697f5449
Luis Kcomt <45114652+kcomt@users.noreply.github.com>
2025-01-30 17:26:01 -0500

    netlify

---
+++ src/functions/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const cors = require("cors");
+const scrapeRoutes = require("../src/routes/scrapeRoutes");
+
+const app = express();
+
+// Middleware
+const corsOptions = {
+  origin: "https://rapidapi.com",
+  methods: "GET,POST",
+  allowedHeaders: ["x-api-key"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// Routes
+app.use("/", scrapeRoutes);
+
+module.exports.handler = serverless(app);
